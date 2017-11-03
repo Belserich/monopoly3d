d7817d30744b18b68f5ddb59674b444962e9cf8f
Markus Uhlig <markus.uhlig@b-tu.de>
2017-11-03 11:27:07 +0100

    Introduced project structure.

---
+++ src/main/java/de/btu/monopoly/Launcher.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.btu.monopoly;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Markus Uhlig (markus.uhlig@b-tu.de)
+ */
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+
+        stage.setTitle("Softwarepraktikum WS 17/18");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application. main() serves only as fallback in case the
+     * application can not be launched through deployment artifacts, e.g., in IDEs with limited FX support. NetBeans ignores
+     * main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
