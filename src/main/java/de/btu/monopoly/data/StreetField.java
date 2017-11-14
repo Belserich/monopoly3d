f28fae486ccf8040aa19b02e0ffeb98a7b6cc5a8
Christian Prinz <prinzach@b-tu.de>
2017-11-14 10:15:17 +0100

    Kommentare hinzugefügt

--- src/main/java/de/btu/monopoly/data/StreetField.java
+++ src/main/java/de/btu/monopoly/data/StreetField.java
@@ -1,27 +1,46 @@
 package de.btu.monopoly.data;
 
 /**
- * Created by Belserich Gremory on 13/11/2017.
+ * Created by Belserich Gremory on 13/11/2017. Ein Strassenfeld
  */
 public class StreetField extends Property {
-    
+
+    /**
+     * die Miete der Straße, abhänging vom Bebauungsstatus
+     */
     private final int[] rents;
+    /**
+     * der Preis für ein Haus
+     */
     private final int housePrice;
-    
+    /**
+     * Anzahl der Haeuser auf der Strasse
+     */
     private int houseCount;
-    
+
+    /**
+     *
+     * @param name - Strassenname
+     * @param price - Kaufpreis
+     * @param rent0 - Miete unbebaut
+     * @param rent1 - Miete mit 1 Haus
+     * @param rent2 - Miete mit 2 Haeusern
+     * @param rent3 - Miete mit 3 Haeusern
+     * @param rent4 - Miete mit 4 Haeusern
+     * @param rent5 - Miete mit Hotel
+     * @param housePrice - Preis fuer ein Haus
+     */
     public StreetField(String name,
-                       int price,
-                       int rent0,
-                       int rent1,
-                       int rent2,
-                       int rent3,
-                       int rent4,
-                       int rent5,
-                       int housePrice)
-    {
+            int price,
+            int rent0,
+            int rent1,
+            int rent2,
+            int rent3,
+            int rent4,
+            int rent5,
+            int housePrice) {
         super(name, price);
-        
+
         this.rents = new int[6];
         this.rents[0] = rent0;
         this.rents[1] = rent1;
@@ -30,22 +49,31 @@ public class StreetField extends Property {
         this.rents[4] = rent4;
         this.rents[5] = rent5;
         this.housePrice = housePrice;
-    
+
         this.houseCount = 0;
-        
+
     }
-    
+
+    /**
+     * @return aktuelle Miete
+     */
     public int getRent() {
         if (!isMortgageTaken()) {
             return rents[houseCount];
         }
         return -1;
     }
-    
+
+    /**
+     * @return Preis für ein Haus
+     */
     public int getHousePrice() {
         return housePrice;
     }
-    
+
+    /**
+     * @return Anzahl der Haeuser
+     */
     public int getHouseCount() {
         return houseCount;
     }
