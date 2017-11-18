cdc1994eec1b49dcf2b807b228b66d6fca24d201
Maximilian Bels <belsmaxi@b-tu.de>
2017-11-18 13:28:19 +0000

    Jedem Feld wird eine eindeutige Identifikationsnummer gegeben.

--- src/main/java/de/btu/monopoly/data/Field.java
+++ src/main/java/de/btu/monopoly/data/Field.java
@@ -1,25 +1,33 @@
 package de.btu.monopoly.data;
 
 public abstract class Field {
-
+    
     /**
      * Name des Feldes
      */
     private final String name;
-
+    
     /**
-     *
+     * Die Id des Felds
+     */
+    private final int id;
+    
+    /**
      * @param name des Feldes
      */
-    public Field(String name) {
+    public Field(String name, int id) {
         this.name = name;
+        this.id = id;
     }
-
+    
     /**
-     *
      * @return Name des Feldes
      */
     public String getName() {
         return name;
+    }
+    
+    public int getId() {
+        return id;
     }
 }
