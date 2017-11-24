dcf0a249d685c093eb254ffe63209ae55a441f8c
Maximilian Bels <belsmaxi@b-tu.de>
2017-11-24 03:23:33 +0000

    - card_data wurde aufgeteilt + field_data auf neues Kommentarsymbol geupdated + ParserUtils Klasse als zentrale Utility angelegt

---
+++ src/main/java/de/btu/monopoly/data/parser/ParserUtils.java
@@ -0,0 +1,42 @@
+package de.btu.monopoly.data.parser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author Maximilian Bels (belsmaxi@b-tu.de)
+ */
+public class ParserUtils {
+    
+    /**
+     * Kommentarsymbol
+     */
+    public static final String C = "//";
+    
+    /**
+     * Argumentseparator (darf kein Steuerzeichen sein)
+     */
+    public static final String S = ",";
+    
+    /**
+     * Reduziert den Inhalt einer Textdatei auf seine Informationen.
+     *
+     * @param path Der Pfad der Textdatei
+     * @return Die Zeilen der Textdatei, frei von Steuerzeichen und unter Ausschluss leerer Zeilen.
+     * @throws IOException Datei ist nicht lesbar, nicht vorhanden oder beschädigt.
+     */
+    public static String[] trimInsignificant(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(GameBoardParser.class.getClassLoader().getResourceAsStream(path)));
+        String[] retObj = reader.lines()
+                .map(s -> {
+                    int i = s.indexOf(C);
+                    return i == -1 ? s : s.substring(0, i);
+                }) // trims comments
+                .map(s -> s.replaceAll("[^\\w|" + S + "]", "")) // trimmt alle Kontrollsymbole
+                .filter(s -> s.length() > 0) // trimmt leere Zeilen
+                .toArray(String[]::new); // erstellt ein neues Array und packt alle verbliebenen Zeilen hinein.
+        reader.close();
+        return retObj;
+    }
+}
