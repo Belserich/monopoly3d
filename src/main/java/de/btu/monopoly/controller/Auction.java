b4cf3a0bfe7d6742db606c41ab07c91b0c34891a
Christian Prinz <prinzach@b-tu.de>
2017-11-25 17:10:12 +0100

    + betphase im GameController abgeschlossen. Implementierung in Auction

---
+++ src/main/java/de/btu/monopoly/controller/Auction.java
@@ -0,0 +1,54 @@
+package de.btu.monopoly.controller;
+
+import de.btu.monopoly.data.Player;
+import de.btu.monopoly.data.field.Property;
+
+/**
+ * Eine Klasse die zur Auktion von Grundstuecken dient
+ *
+ * @author Christian Prinz
+ */
+public class Auction {
+
+    private int price;
+
+    private Player[] players;
+
+    private Player winner;
+
+    private Property property;
+
+    /**
+     *
+     * @param property Die Strasse die versteigert werden soll
+     */
+    public Auction(Property property, Player[] players) {
+        this.property = property;
+        this.players = players;
+    }
+
+    public void startAuction() {
+        /*
+         * TODO hier findet die Auktion statt. Resultat der Auktion ist, dass ein Gewinner gefunden und anschliessend der Preis,
+         * sowie der Gewinner in der Klasse gespeichert werden.
+         */
+        //PLATZHALTER:
+        this.price = 0;
+        this.winner = players[0];
+    }
+
+    /**
+     * @return the price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * @return the winner
+     */
+    public Player getWinner() {
+        return winner;
+    }
+
+}
