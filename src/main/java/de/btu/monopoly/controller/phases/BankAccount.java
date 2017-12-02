ca346b3a06ae107091e1d60b707c6ef00457b354
Maximilian Bels <belsmaxi@b-tu.de>
2017-12-02 20:22:13 +0100

    + BankAccount Klasse als Handlerklasse für das Kapital des Spielers

---
+++ src/main/java/de/btu/monopoly/controller/phases/BankAccount.java
@@ -0,0 +1,37 @@
+package de.btu.monopoly.controller.phases;
+
+import java.util.logging.Logger;
+
+public class BankAccount {
+
+    private int balance;
+
+    public BankAccount(int startMoney) {
+        this.balance = startMoney;
+    }
+
+    public boolean checkLiquidity(int loss) {
+        return (balance - loss >= 0);
+    }
+
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public boolean isLiquid() {
+        return balance >= 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Bank-Account] Balance: %d", balance);
+    }
+}
