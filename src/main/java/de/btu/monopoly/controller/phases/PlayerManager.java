6e60b35af997eaa8960bd818909882a8844f0a71
Maximilian Bels <belsmaxi@b-tu.de>
2017-12-02 20:25:24 +0100

    + PlayerManager um statische Hilfemethoden erweitert

--- src/main/java/de/btu/monopoly/controller/phases/PlayerManager.java
+++ src/main/java/de/btu/monopoly/controller/phases/PlayerManager.java
@@ -1,20 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.btu.monopoly.controller.phases;
 
-import static de.btu.monopoly.controller.GameController.CURRENCY_TYPE;
-import static de.btu.monopoly.controller.GameController.logger;
-import de.btu.monopoly.data.GameBoard;
 import de.btu.monopoly.data.Player;
-import de.btu.monopoly.data.field.Field;
-import de.btu.monopoly.data.field.GoField;
-import de.btu.monopoly.data.field.Property;
-import de.btu.monopoly.data.field.StreetField;
+
 import java.util.Arrays;
-import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,73 +11,53 @@ import java.util.logging.Level;
  */
 public class PlayerManager {
 
-    private GameBoard board;
-    private Player[] players;
-
-    public PlayerManager(GameBoard board, Player[] players) {
-        this.board = board;
-        this.players = players;
-    }
+    private static final Logger LOGGER = Logger.getLogger(PlayerManager.class.getCanonicalName());
 
     /**
-     * das Wuerfeln. Ergebnisse werden lokal in rollResult und doubletCounter gespeichert
-     */
-    public int[] roll(Player player) {
-        int[] rollResult = new int[2];
-        // Erzeugen der Zufallszahl
-        rollResult[0] = ((int) (Math.random() * 6)) + 1;
-        rollResult[1] = ((int) (Math.random() * 6)) + 1;
-
-        logger.log(Level.INFO, player.getName() + " würfelt " + rollResult[0] + " + " + rollResult[1]
-                + " = " + (rollResult[0] + rollResult[1]));
-
-        return rollResult;
-    }
-
-    /**
-     * bewegt den Spieler zu einer neuen Position.
+     * Setzt alle nötigen Attribute, wenn der Spieler ins Gefängnis kommt. Bitte {@code FieldManager.toJail()} benutzen.
      *
-     * @param player Spieler der bewegt wird
+     * @param player Spieler
      */
-    public void movePlayer(Player player, int fields) {
-        if (player.getPosition() + fields > 39) {
-            logger.log(Level.FINE, player.getName() + " hat das \"LOS\"-Feld passiert und erhaelt "
-                    + ((GoField) board.getFields()[0]).getAmount() + " " + CURRENCY_TYPE + ".");
-            giveMoney(player, ((GoField) board.getFields()[0]).getAmount());
-
-            player.setPosition(player.getPosition() + fields - 39);
-        } else {
-            player.setPosition(player.getPosition() + fields);
-        }
-    }
-
-    /**
-     * bewegt den Spieler ins Gefängnis und setzt seine Attribute entsprechend
-     *
-     * @param player Spieler der ins Gefaengnis kommt
-     */
-    public void moveToJail(Player player) {
-        /*
-         * Spieler wird auf Position 10 gesetzt setInJail wird true, damit der Spieler nicht "nur zu Besuch" ist Die Tage im
-         * Gefängnis werden auf 0 gesetzt
-         */
-        player.setPosition(10);
+    static void toJail(Player player) {
         player.setInJail(true);
         player.setDaysInJail(0);
-        logger.log(Level.INFO, player.getName() + " ist jetzt im Gefaengnis.");
+        LOGGER.info(String.format("%s ist jetzt im Gefaengnis.", player.getName()));
     }
 
     /**
-     * ueberpruft ob der uebergebene Spieler mindestens soviel Geld besitzt, wie die Methode uebergeben bekommt.
+     * Setzt das Positionsindex für den Spieler. Bitte {@code FieldManager.movePlayer()} benutzen.
      *
-     * @param player Spieler der auf Liquiditaet geprueft wird
-     * @param amount Geld was der Spieler besitzen muss
+     * @param player Spieler
+     * @param amount Anzahl der zu laufenden Felder
+     * @return Die neue Position des Spielers auf dem Feld.
      */
-    public boolean checkLiquidity(Player player, int amount) {
+    static int movePlayer(Player player, int amount) {
+        int pos = player.getPosition();
+        pos += amount;
+        player.setPosition(pos);
+        if (amount >= 0) {
+            LOGGER.info(String.format("%s wurde %d Felder weiter bewegt.", player.getName(), amount));
+        }
+        return pos;
+    }
 
-        logger.log(Level.INFO, "Es wird geprüft, ob " + player.getName() + " genug Geld hat für die Transaktion.");
-        return (player.getMoney() - amount) > 0;
-
+    /**
+     * Prüft die Zahlungsfähigkeit eines Spielers.
+     *
+     * @param player Spieler
+     * @param amount Anzahl Geldeinheiten
+     * @return true, wenn zahlungsfähig, sonst false
+     */
+    public static boolean checkLiquidity(Player player, int amount) {
+        BankAccount bank = player.getBankAccount();
+        if (bank.checkLiquidity(amount)) {
+            LOGGER.fine("Spielerkonto auf Liquidität geprüft. Er besitzt genug Geld.");
+            return true;
+        }
+        else {
+            LOGGER.warning("Spielerkonto auf Liquidität geprüft. Er besitzt nicht genug Geld!");
+            return false;
+        }
     }
 
     /**
@@ -97,17 +66,13 @@ public class PlayerManager {
      * @param player Spieler dem der Betrag abgezogen wird
      * @param amount Betrag der dem Spieler abgezogen wird
      */
-    public void takeMoney(Player player, int amount) {
-
-        player.setMoney(player.getMoney() - amount);
-        logger.log(Level.INFO, player.getName() + " werden " + amount + CURRENCY_TYPE + " abgezogen.");
-
-        if (amount < 0) {
-            bankrupt(player);
+    public static void takeMoneyUnchecked(Player player, int amount) {
+        BankAccount bank = player.getBankAccount();
+        bank.withdraw(amount);
+        LOGGER.info(String.format("%s (+%d)", bank.toString(), amount));
+        if (!bank.isLiquid()) {
+            LOGGER.info(String.format("%s hat sich verschuldet!", player.getName()));
         }
-
-        logger.log(Level.INFO, "Der Kontostand von " + player.getName() + " beträgt nun: " + player.getMoney() + CURRENCY_TYPE + ".");
-
     }
 
     /**
@@ -116,99 +81,18 @@ public class PlayerManager {
      * @param player Spieler dem der Betrag gutgeschrieben wird
      * @param amount Betrag der dem Spieler gutgeschrieben wird
      */
-    public void giveMoney(Player player, int amount) {
-
-        player.setMoney(player.getMoney() + amount);
-        logger.log(Level.INFO, player.getName() + " erhält " + amount + CURRENCY_TYPE + ".");
-        logger.log(Level.INFO, "Der Kontostand von " + player.getName() + " beträgt nun: " + player.getMoney() + CURRENCY_TYPE + ".");
+    public static void giveMoney(Player player, int amount) {
+        BankAccount bank = player.getBankAccount();
+        bank.deposit(amount);
+        LOGGER.info(String.format("%s (-%d)", bank.toString(), amount));
     }
 
     /**
-     * Macht einen Spieler zum Beobachter und entfernt all seinen Besitz!
-     *
-     * @param player Spieler der bankrott gegangen ist
-     * @return ob das Spiel vorbei ist (gameOver)
+     * @return Anzahl der Spieler die nicht Pleite sind
      */
-    public boolean bankrupt(Player player) {
-        boolean gameOver = false; //TODO aufrufe verändern
-
-        logger.log(Level.INFO, player.getName() + " ist Bankrott und ab jetzt nur noch Zuschauer. All sein Besitz geht zurück an die Bank.");
-
-        // Spieler als Zuschauer festlegen
-        player.setSpectator(true);
-
-        // Temporär das Feldarray zum Durchgehen zwischenspeichern
-        Field[] fields = board.getFields();
-
-        // geht ein Spieler bankrott wird geprueft, ob er der vorletzte war
-        if (countActivePlayers() <= 1) {
-            gameOver = true;
-        }
-
-        // Durchgehen des Array fields, ggf. Eigentum löschen
-        for (int i = 0; i < fields.length; i++) {
-            Field field = fields[i];
-
-            // Löschen der Hypothek und des Eigentums
-            if (field instanceof Property) {
-                if (((Property) field).getOwner() == player) {
-                    ((Property) field).setOwner(null);
-                    ((Property) field).setMortgageTaken(false);
-                }
-
-                // Löschen der Anzahl an Häusern
-                if (field instanceof StreetField) {
-                    ((StreetField) fields[i]).setHouseCount(0);
-
-                }
-            }
-        }
-        return gameOver;
-        // TODO @cards - Gefängnisfreikarten müssen zurück in den Stapel
-    }
-
-    /**
-     *
-     * @return Anzahl der Spieler die nicht pleite sind
-     */
-    public int countActivePlayers() {
+    public static int countActive(Player[] players) {
         return (int) Arrays.stream(players)
-                .filter(p -> !(p.isSpectator()))
+                .filter(p -> !(p.isBankrupt()))
                 .count();
     }
-
-    /**
-     * Alle Gebaeude eines Spielers werden gezaehlt
-     *
-     * Die Preise fuer Renovierung werden von dem entsprechenden Karte bekannt und dies wird mit der Anzahl von Haeuser/Hotels
-     * multipliziert und am Ende addiert = Summe
-     *
-     * @param housePrice Hauspreis
-     * @param hotelPrice Hotelpreis
-     */
-    public void sumRenovation(Player player, int housePrice, int hotelPrice) {
-        //TODO spaeter, wenn Kartenstapel gedruckt wurde
-
-        int renovationHotel = 0;
-        int renovationHouse = 0;
-        for (Field field : board.getFields()) {
-            if (field instanceof StreetField) {
-                int houses = ((StreetField) field).getHouseCount();
-                if (houses < 5) {
-                    renovationHouse += (housePrice * houses);
-
-                } else {
-                    renovationHotel += hotelPrice;
-
-                }
-            }
-        }
-        int sum = renovationHouse + renovationHotel;
-        if (checkLiquidity(player, sum)) {
-            takeMoney(player, sum);
-        } else {
-            bankrupt(player);
-        }
-    }
-
 }
