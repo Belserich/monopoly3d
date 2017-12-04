885019e970067fc5427fd85c524d69bd312d3806
tiesn <pcprinz26@gmail.com>
2017-12-04 15:40:52 +0100

    +Serverstruktur angefangen +MainTest in Testpackage verschoben (temporär)

---
+++ src/main/java/de/btu/monopoly/net/GameClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.btu.monopoly.net;
+
+import com.esotericsoftware.kryonet.Client;
+
+/**
+ *
+ * @author Christian Prinz
+ */
+public class GameClient {
+
+    private int tcpPort;
+    private int udpPort;
+    private int timeout;
+    private Client client;
+
+}
