fe0875e2180dca61d19dc8d5aeb46f23d9120e27
Maximilian Bels <belsmaxi@b-tu.de>
2017-12-06 16:19:21 +0000

    + chat-package erstellt + chat-feature in chat-Package verschoben

--- src/main/java/de/btu/monopoly/net/chat/ServerListener.java
+++ src/main/java/de/btu/monopoly/net/chat/ServerListener.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.btu.monopoly.net;
+package de.btu.monopoly.net.chat;
 
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
