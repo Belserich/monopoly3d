aa1282cc7ade13e709b59e69559a4cc0ce607755
Maximilian Bels <belsmaxi@b-tu.de>
2017-12-07 11:30:40 +0000

    + modifizierter Launcher fürs Review

--- src/main/java/de/btu/monopoly/Launcher.java
+++ src/main/java/de/btu/monopoly/Launcher.java
@@ -5,16 +5,18 @@
  */
 package de.btu.monopoly;
 
-import de.btu.monopoly.menu.MainMenu;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import de.btu.monopoly.core.Game;
+import de.btu.monopoly.data.player.Player;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  *
@@ -45,19 +47,21 @@ public class Launcher extends Application {
     public static void main(String[] args) throws Exception {
         configLoggers();
         initGame();
-        launch(args);
+        // launch(args);
     }
 
     public static void initGame() {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.start();
+        Player[] players = new Player[4];
+        for (int i = 0; i < 4; i++) {
+            Player player = new Player("Mathias " + (i + 1), i, 1500);
+            players[i] = player;
+        }
+    
+        Game game = new Game(players);
+        game.init();
+        game.start();
     }
-
-//    private static void initGame() {
-//        Game controller = new Game(2);
-//        controller.init();
-//        controller.start();
-//    }
+    
     private static void configLoggers() {
         try {
             LogManager.getLogManager().readConfiguration(Launcher.class.getResourceAsStream("/data/config/logging.properties"));
