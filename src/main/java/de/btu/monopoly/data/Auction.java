1e88ed931cd2e8ee4b54e7af9fd40640ae39ecdf
patrick <patrick@Patricks-ThinkPad>
2017-12-13 20:09:01 +0100

    +playerExit +getHighestBid

--- src/main/java/de/btu/monopoly/data/Auction.java
+++ src/main/java/de/btu/monopoly/data/Auction.java
@@ -4,9 +4,9 @@ import de.btu.monopoly.data.field.PropertyField;
 import de.btu.monopoly.data.player.Player;
 
 /**
- * Eine Klasse die zur Auktion von Grundstuecken dient
+ * Auktions Klasse Auktionen zum ersteigern von Grundstuecken, welche zuvor nicht gekauft wurden.
  *
- * @author Christian Prinz
+ * @author Patrick Kalweit
  */
 public class Auction {
 
@@ -32,13 +32,72 @@ public class Auction {
      * Startet die Auktion, ermittelt den Höchstbietenden und übergibt den Gewinner, sowie den Preis an deren Klassenvariablen
      */
     public void startAuction() {
+
+        int playerNumb = this.players.length;
+
         /*
-         * TODO hier findet die Auktion statt. Resultat der Auktion ist, dass ein Gewinner gefunden und anschliessend der Preis,
-         * sowie der Gewinner in der Klasse gespeichert werden. Hier wird kein Besitz übertragen und kein Geld abgezogen!!!
+        Erstelle ein int[][] welches die ID's und Gebote der Spieler speichert
          */
+        int[][] aucPlayers = new int[playerNumb][2];
+        for (int i = 0; i < playerNumb; i++) {
+            aucPlayers[i][0] = players[i].getId();
+            aucPlayers[i][1] = 0;
+        }
+
         //PLATZHALTER:
         this.price = 0;
         this.winner = players[0];
+    }
+
+    /**
+     * Diese Methode ermoeglicht es einem Spieler, die Auktion zu verlassen.
+     *
+     * @param activePlayers
+     * @return
+     */
+    public int[][] playerExit(int[][] activePlayers, int id) {
+
+        //neues und kleineres Spieler Array
+        int playerNumb = activePlayers.length;
+        int[][] refreshedPlayers = new int[playerNumb][2];
+
+        //geht jeden Platz des neuen Arrays durch
+        for (int i = 0; i < refreshedPlayers.length; i++) {
+            //verschiebt nach und nach die aktiven Spieler in das neue Array
+            for (int j = 0; j < activePlayers.length; j++) {
+                if (activePlayers[j][0] != -1) {
+                    if (activePlayers[j][0] != id) {
+                        refreshedPlayers[i][0] = activePlayers[j][0];
+                        activePlayers[j][0] = -1;
+                        refreshedPlayers[i][1] = activePlayers[j][1];
+                        break;
+                    }
+                }
+            }
+        }
+
+        return refreshedPlayers;
+    }
+
+    /**
+     * Das hoechste Gebot aller Bieter wird ermittelt. Es wird die ID des Spielers mit dem hoechsten Gebot zurueck gegeben
+     *
+     * @param activePlayers
+     * @return playerID
+     */
+    public int getHighestBid(int[][] activePlayers) {
+
+        int playerID = -1;
+        int highestBid = -1;
+
+        for (int i = 0; i < activePlayers.length; i++) {
+            if (highestBid < activePlayers[i][1] && activePlayers[i][1] != 0) {
+                playerID = activePlayers[i][0];
+                highestBid = activePlayers[i][1];
+            }
+        }
+
+        return playerID;
     }
 
     /**
