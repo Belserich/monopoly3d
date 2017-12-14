b6ac60c16cdc3fa2135ce8b7bb1c26ed6c299fce
tiesn <pcprinz26@gmail.com>
2017-12-14 12:47:42 +0100

    + Bugfixes

--- src/main/java/de/btu/monopoly/core/Game.java
+++ src/main/java/de/btu/monopoly/core/Game.java
@@ -14,7 +14,6 @@ import de.btu.monopoly.data.player.Player;
 import de.btu.monopoly.input.IOService;
 import de.btu.monopoly.input.InputHandler;
 import de.btu.monopoly.net.client.GameClient;
-import de.btu.monopoly.net.networkClasses.BroadcastPlayerChoiceRequest;
 import de.btu.monopoly.net.networkClasses.PlayerTradeRequest;
 import java.io.IOException;
 import java.util.List;
@@ -149,30 +148,6 @@ public class Game {
                     break;
             }
         } while (player.isInJail() && choice != 1);
-    }
-
-    private int getClientChoice(Player player, int max) {
-
-        if (isChoiceFromThisClient(player)) {
-            int choice = InputHandler.getUserInput(max);
-            BroadcastPlayerChoiceRequest packet = new BroadcastPlayerChoiceRequest();
-            packet.setChoice(choice);
-            client.sendTCP(packet);
-            return choice;
-        } else {
-            do {
-                BroadcastPlayerChoiceRequest[] packets = client.getPlayerChoiceObjects();
-                if (packets.length > 1) {
-                    LOGGER.warning("Fehler: Mehr als ein choice-Packet registriert!");
-                    return -1;
-                } else if (packets.length == 1) {
-                    int retVal = packets[0].getChoice();
-                    client.clearPlayerChoiceObjects();
-                    return retVal;
-                }
-            } while (getIS_RUNNING().get());
-        }
-        return -1;
     }
 
     private boolean isChoiceFromThisClient(Player player) {
