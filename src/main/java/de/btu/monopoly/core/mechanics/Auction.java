d17e0ba54912d1cffaeda61cd99055a49cb77567
patrick <patrick@Patricks-ThinkPad>
2017-12-14 19:25:35 +0100

    +getPlayers -startAuction (da ausgelagert)

--- src/main/java/de/btu/monopoly/core/mechanics/Auction.java
+++ src/main/java/de/btu/monopoly/core/mechanics/Auction.java
@@ -14,8 +14,6 @@ public class Auction {
 
     private Player[] players;
 
-    private Player winner;
-
     private PropertyField property;
 
     /**
@@ -29,37 +27,12 @@ public class Auction {
     }
 
     /**
-     * Startet die Auktion, ermittelt den Höchstbietenden und übergibt den Gewinner, sowie den Preis an deren Klassenvariablen
+     * gibt das Player[] zurueck
+     *
+     * @return
      */
-    public void startAuction() {
-
-        int activCount = 0;
-        int playerNumb = this.players.length;
-
-        /*
-        Erstelle ein int[][] welches die ID's und Gebote der Spieler speichert
-         */
-        int[][] aucPlayers = new int[playerNumb][3];
-        for (int i = 0; i < playerNumb; i++) {
-            aucPlayers[i][0] = players[i].getId();      //Speicher Spieler ID
-            aucPlayers[i][1] = 0;                       //Speichert Spieler Gebot
-            aucPlayers[i][2] = 1;                       //Spieler noch aktiv? 1 = ja, 0 = nein
-        }
-
-        do {
-
-            /*
-            Spielogik
-             */
-            //for Schleife zur Ueberpruefung der aktiven Spieleranzahl
-            for (int i = 0; i < aucPlayers.length; i++) {
-                activCount += aucPlayers[i][2];
-            }
-        } while (activCount > 1);
-
-        //PLATZHALTER:
-        this.price = 0;
-        this.winner = players[0];
+    public Player[] getPlayers() {
+        return this.players;
     }
 
     /**
