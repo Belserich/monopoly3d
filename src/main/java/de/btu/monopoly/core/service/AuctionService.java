0f36ad4c460d612665469103f90c2d7a0cf619b4
patrick <patrick@Patricks-ThinkPad>
2017-12-14 19:33:58 +0100

    +setBid() +playerExit() +getHighestBid() (alle aus Auction)

--- src/main/java/de/btu/monopoly/core/service/AuctionService.java
+++ src/main/java/de/btu/monopoly/core/service/AuctionService.java
@@ -12,7 +12,7 @@ public class AuctionService {
     /**
      * Startet die Auktion, ermittelt den Höchstbietenden und übergibt den Gewinner, sowie den Preis an deren Klassenvariablen
      */
-    public void startAuction(Auction auc) {
+    public static void startAuction(Auction auc) {
 
         int activCount = 0;
         Player[] players = auc.getPlayers();
@@ -39,6 +39,52 @@ public class AuctionService {
             }
         } while (activCount > 1);
 
+    }
+
+    /**
+     * Setzt das Gebot eines Spielers
+     *
+     * @param activePlayers
+     * @param i
+     * @param bid
+     */
+    private void setBid(int[][] activePlayers, int i, int bid) {
+
+        activePlayers[i][1] = bid;
+    }
+
+    /**
+     * Diese Methode ermoeglicht es einem Spieler, die Auktion zu verlassen.
+     *
+     * @param activePlayers
+     * @return
+     */
+    private int[][] playerExit(int[][] activePlayers, int i) {
+
+        activePlayers[i][2] = 0;
+
+        return activePlayers;
+    }
+
+    /**
+     * Das hoechste Gebot aller Bieter wird ermittelt. Es wird die ID des Spielers mit dem hoechsten Gebot zurueck gegeben
+     *
+     * @param activePlayers
+     * @return playerID
+     */
+    private int getHighestBid(int[][] activePlayers) {
+
+        int playerID = -1;
+        int highestBid = -1;
+
+        for (int i = 0; i < activePlayers.length; i++) {
+            if (highestBid < activePlayers[i][1] && activePlayers[i][1] != 0) {
+                playerID = activePlayers[i][0];
+                highestBid = activePlayers[i][1];
+            }
+        }
+
+        return playerID;
     }
 
 }
