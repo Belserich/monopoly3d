6bc9403ca78ff4d2958d23dd8165083ba87341c6
Eleonora Kostova <kostoele@b-tu.de>
2017-12-15 11:18:25 +0100

    mainScene.fxml - fx:id hinzugefuegt

--- src/main/java/de/btu/monopoly/ui/controller/mainSceneController.java
+++ src/main/java/de/btu/monopoly/ui/controller/mainSceneController.java
@@ -5,6 +5,9 @@
  */
 package de.btu.monopoly.ui.controller;
 
+import de.btu.monopoly.core.Game;
+import de.btu.monopoly.core.GameBoard;
+import de.btu.monopoly.data.player.Player;
 import de.btu.monopoly.menu.Lobby;
 import de.btu.monopoly.net.client.GameClient;
 import de.btu.monopoly.ui.SceneManager;
@@ -20,9 +23,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -32,6 +37,12 @@ import javafx.stage.Stage;
  */
 public class mainSceneController implements Initializable {
 
+    private Game game;
+    private GameBoard board;
+    private Player[] players;
+    public static GameClient client;
+
+    //Spieler ID
     @FXML
     private Label playerMoney1;
     @FXML
@@ -47,10 +58,89 @@ public class mainSceneController implements Initializable {
     @FXML
     private Button player5Button;
 
-    public static GameClient client;
+    //Felder ID
+    @FXML
+    private Pane goField;
+    @FXML
+    private Pane badStr;
+    @FXML
+    private Pane gemenschaft1;
+    @FXML
+    private Pane turmStr;
+    @FXML
+    private Pane einkommenSt;
+    @FXML
+    private Pane suedBahnhof;
+    @FXML
+    private Pane chauseeStr;
+    @FXML
+    private Pane ereignis1;
+    @FXML
+    private Pane postStr;
+    @FXML
+    private Pane besuch;
+    @FXML
+    private Pane seeStr;
+    @FXML
+    private Pane elWerk;
+    @FXML
+    private Pane hafenStr;
+    @FXML
+    private Pane neueStr;
+    @FXML
+    private Pane westBahnhof;
+    @FXML
+    private Pane muenchenerStr;
+    @FXML
+    private Pane gemeinschaft2;
+    @FXML
+    private Pane wienerStr;
+    @FXML
+    private Pane berlinerStr;
+    @FXML
+    private Pane parkplatz;
+    @FXML
+    private Pane theaterStr;
+    @FXML
+    private Pane museumStr;
+    @FXML
+    private Pane opernplatz;
+    @FXML
+    private Pane nordBahnhof;
+    @FXML
+    private Pane lessingStr;
+    @FXML
+    private Pane wasserWerk;
+    @FXML
+    private Pane schillerStr;
+    @FXML
+    private Pane goetheStr;
+    @FXML
+    private Pane jailField;
+    @FXML
+    private Pane rathhausPlatz;
+    @FXML
+    private Pane hauptStr;
+    @FXML
+    private Pane gemeinschat3;
+    @FXML
+    private Pane bahnhofStr;
+    @FXML
+    private Pane hauptBahnhof;
+    @FXML
+    private Pane ereignis3;
+    @FXML
+    private Pane parkStr;
+    @FXML
+    private Pane zusatzSt;
+    @FXML
+    private Pane schlossAllee;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        game = new Game(players, client, 0);
+        board = game.getBoard(); 
         // User aus Lobby holen
         if (Lobby.getUsers() != null) {
             if (Lobby.getUsers().length >= 1) {
@@ -58,18 +148,28 @@ public class mainSceneController implements Initializable {
             }
             if (Lobby.getUsers().length >= 2) {
                 player1Button.setText(Lobby.getUsers()[1][1]);
+            } else {
+                player1Button.setText("");
             }
             if (Lobby.getUsers().length >= 3) {
                 player2Button.setText(Lobby.getUsers()[2][1]);
+            } else {
+                player2Button.setText("");
             }
             if (Lobby.getUsers().length >= 4) {
                 player3Button.setText(Lobby.getUsers()[3][1]);
+            } else {
+                player3Button.setBlendMode(BlendMode.valueOf(""));
             }
             if (Lobby.getUsers().length >= 5) {
                 player4Button.setText(Lobby.getUsers()[4][1]);
+            } else {
+                player4Button.setText("");
             }
             if (Lobby.getUsers().length >= 6) {
                 player5Button.setText(Lobby.getUsers()[5][1]);
+            } else {
+                player5Button.setText("");
             }
         }
 
@@ -83,6 +183,12 @@ public class mainSceneController implements Initializable {
             player5Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[5][4]), CornerRadii.EMPTY, Insets.EMPTY)));
 
         }
+        //TODO
+//        if(players[0].getPosition()== Integer.parseInt(board.getFields()[0].toString())){
+//       
+//    }
+//        goField.setId(board.getFields()[0].toString());
+//        badStr.setId(board.getFields()[1].toString());
     }
 
     public void changeName(ActionEvent event) throws IOException {
