a256b7a3e15980a7dbf6bfea7fc1cfed4dd15771
John Augat <augatjoh@b-tu.de>
2017-12-15 21:11:44 +0100

    Optische Anpassung der Menu, JoinGame und StartGame Scenen

--- src/main/java/de/btu/monopoly/ui/controller/JoinGameController.java
+++ src/main/java/de/btu/monopoly/ui/controller/JoinGameController.java
@@ -19,6 +19,7 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 /**
  *
@@ -38,11 +39,14 @@ public class JoinGameController implements Initializable {
     @FXML
     private GridPane grid;
 
+    @FXML
+    private StackPane stackPane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         Image image = new Image("https://cdn.vox-cdn.com/thumbor/LuyPPsKiSwRkL0i87Ur-8GDhSDM=/0x0:1144x566/1200x800/filters:focal(481x192:663x374)/cdn.vox-cdn.com/uploads/chorus_image/image/52679863/Screen_Shot_2017_01_10_at_10.41.40_AM.0.png");
         grid.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-
+        stackPane.setBackground(new Background(new BackgroundImage(new Image("https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/di3a2-ACJM5H51YKB-Full-Image_GalleryBackground-en-US-1489722831648._RI_SX940_.jpg"), BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
     }
 
     // Button back
@@ -68,7 +72,7 @@ public class JoinGameController implements Initializable {
             // Wechselt die Scene auf Lobby
             SceneManager.changeSceneToLobby(new FXMLLoader(getClass().getResource("/fxml/Lobby.fxml")));
         } else {
-            errorLabel.setText("Die Verbindung konnte nicht hergestellt werden.");
+            errorLabel.setText("Keine Verbindung möglich.");
         }
 
     }
