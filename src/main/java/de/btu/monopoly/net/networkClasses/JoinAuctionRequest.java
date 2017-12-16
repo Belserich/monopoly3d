3f0c9ad9d81134e9e4f84ed9f06e43d9b3e6f1e4
Patrick <kalwepat@b-tu.de>
2017-12-16 14:40:28 +0100

    +JoinAuctionRequest.java

---
+++ src/main/java/de/btu/monopoly/net/networkClasses/JoinAuctionRequest.java
@@ -0,0 +1,12 @@
+package de.btu.monopoly.net.networkClasses;
+
+/**
+ *
+ * @author patrick
+ */
+public class JoinAuctionRequest {
+
+    /*
+    Leeres Paket, als Startsignal fuer die AuctionLobby
+     */
+}
