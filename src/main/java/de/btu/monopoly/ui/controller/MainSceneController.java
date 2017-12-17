f7d149156cca17ef00109f4cf0d99439b1416748
Eleonora Kostova <kostoele@b-tu.de>
2017-12-17 14:21:58 +0100

    Spieler Figuren hinzugefuegt

--- src/main/java/de/btu/monopoly/ui/controller/MainSceneController.java
+++ src/main/java/de/btu/monopoly/ui/controller/MainSceneController.java
@@ -5,6 +5,8 @@
  */
 package de.btu.monopoly.ui.controller;
 
+import de.btu.monopoly.core.Game;
+import de.btu.monopoly.core.GameBoard;
 import de.btu.monopoly.data.player.Player;
 import de.btu.monopoly.menu.Lobby;
 import de.btu.monopoly.net.client.GameClient;
@@ -21,28 +23,52 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
  *
- * @author Eleonora kostova
+ * @author Eleonora Kostova
  */
 public class MainSceneController implements Initializable {
 
-    // TODO
-//    private Game game;
-//    private GameBoard board;
-    private Player[] players;
-    public static GameClient client;
+    GameClient client;
+    Lobby lobby;
+
+    @FXML
+    private GridPane grid;
+    @FXML
+    private GridPane grid2;
+    @FXML
+    private GridPane gridMiddle;
+
+    //Spieler Figur
+    @FXML
+    private Circle player0;
+    @FXML
+    private Circle player1;
+    @FXML
+    private Circle player2;
+    @FXML
+    private Circle player3;
+    @FXML
+    private Circle player4;
+    @FXML
+    private Circle player5;
 
     //Spieler ID
-    @FXML
-    private Label playerMoney1;
     @FXML
     private Button player0Button;
     @FXML
@@ -137,9 +163,14 @@ public class MainSceneController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        // TODO
-//        game = new Game(players, client, 0);
-//        board = game.getBoard();
+        Image image = new Image(
+                "https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/di3a2-ACJM5H51YKB-Full-Image_GalleryBackground-en-US-1489722831648._RI_SX940_.jpg");
+        grid.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+        grid2.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
+
+        Image image2 = new Image("https://upload.wikimedia.org/wikipedia/en/f/f9/Monopoly_pack_logo.png");
+        gridMiddle.setBackground(new Background(new BackgroundImage(image2, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+
         // User aus Lobby holen
         if (Lobby.getUsers() != null) {
             if (Lobby.getUsers().length >= 1) {
@@ -148,27 +179,32 @@ public class MainSceneController implements Initializable {
             if (Lobby.getUsers().length >= 2) {
                 player1Button.setText(Lobby.getUsers()[1][1]);
             } else {
-                player1Button.setText("");
+                player1Button.setVisible(false);
+                player1.setVisible(false);
             }
             if (Lobby.getUsers().length >= 3) {
                 player2Button.setText(Lobby.getUsers()[2][1]);
             } else {
-                player2Button.setText("");
+                player2Button.setVisible(false);
+                player2.setVisible(false);
             }
             if (Lobby.getUsers().length >= 4) {
                 player3Button.setText(Lobby.getUsers()[3][1]);
             } else {
-                player3Button.setText("");
+                player3Button.setVisible(false);
+                player3.setVisible(false);
             }
             if (Lobby.getUsers().length >= 5) {
                 player4Button.setText(Lobby.getUsers()[4][1]);
             } else {
-                player4Button.setText("");
+                player4Button.setVisible(false);
+                player4.setVisible(false);
             }
             if (Lobby.getUsers().length >= 6) {
                 player5Button.setText(Lobby.getUsers()[5][1]);
             } else {
-                player5Button.setText("");
+                player5Button.setVisible(false);
+                player5.setVisible(false);
             }
         }
 
@@ -176,52 +212,29 @@ public class MainSceneController implements Initializable {
         if (Lobby.getUsers() != null) {
             if (Lobby.getUsers().length >= 1) {
                 player0Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[0][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player0.setFill(Color.web(Lobby.getUsers()[0][4]));
             }
             if (Lobby.getUsers().length >= 2) {
                 player1Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[1][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player1.setFill(Color.web(Lobby.getUsers()[1][4]));
             }
             if (Lobby.getUsers().length >= 3) {
                 player2Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[2][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player2.setFill(Color.web(Lobby.getUsers()[2][4]));
             }
             if (Lobby.getUsers().length >= 4) {
                 player3Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[3][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player3.setFill(Color.web(Lobby.getUsers()[3][4]));
             }
             if (Lobby.getUsers().length >= 5) {
                 player4Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[4][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player4.setFill(Color.web(Lobby.getUsers()[4][4]));
             }
             if (Lobby.getUsers().length >= 6) {
                 player5Button.setBackground(new Background(new BackgroundFill(Color.web(Lobby.getUsers()[5][4]), CornerRadii.EMPTY, Insets.EMPTY)));
+                player5.setFill(Color.web(Lobby.getUsers()[5][4]));
             }
         }
 
-        //TODO
-//        if(players[0].getPosition()== Integer.parseInt(board.getFields()[0].toString())){
-//
-//    }
-//        goField.setId(board.getFields()[0].toString());
-//        badStr.setId(board.getFields()[1].toString());
     }
-
-    public void changeName(ActionEvent event) throws IOException {
-
-        SceneManager.changeScene(new FXMLLoader(getClass().getResource("/fxml/mainSecene.fxml")));
-
-        //playerMoney1.setText(""+client.getPlayerOnClient().getMoney());
-        playerMoney1.setText("1200");
-    }
-
-    public void getPopUpAction() throws IOException {
-        Stage primaryStage;
-        Parent root;
-
-        System.out.println("You pressed mee");
-        primaryStage = new Stage();
-        root = FXMLLoader.load(getClass().getResource("popUpPlayer1.fxml"));
-
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-
 }
