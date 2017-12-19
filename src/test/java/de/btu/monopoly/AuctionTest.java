244785488796d0a29d5d5ec075de9af9101843ad
Patrick <kalwepat@b-tu.de>
2017-12-19 21:02:41 +0100

    +Änderung an Auctiontest --für Chris, bitte gucken

--- src/test/java/de/btu/monopoly/AuctionTest.java
+++ src/test/java/de/btu/monopoly/AuctionTest.java
@@ -1,57 +1,96 @@
-//package de.btu.monopoly;
-//
-////Imports
-//import de.btu.monopoly.core.Game;
-//import de.btu.monopoly.core.GameBoard;
-//import de.btu.monopoly.core.mechanics.Auction;
-//import de.btu.monopoly.core.service.AuctionService;
-//import de.btu.monopoly.data.card.CardManager;
-//import de.btu.monopoly.data.field.*;
-//import de.btu.monopoly.data.player.Player;
-//import de.btu.monopoly.net.client.GameClient;
-//import de.btu.monopoly.net.server.GameServer;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-///**
-// *
-// * @author patrick
-// */
-//public class AuctionTest {
-//
-//    private static Game game;
-//    private static GameBoard board;
-//    private static Player[] players;
-//    private static FieldManager fm;
-//    private static GameServer server;
-//    private static GameClient client;
-//    private static CardManager cm;
-//
-//    public AuctionTest() {
-//
-//        players = new Player[4];
-//        for (int i = 0; i < 2; i++) {
-//            Player player = new Player("root " + (i + 1), i, 1500);
-//            players[i] = player;
-//        }
-//        server = new GameServer(59687);
-//        server.startServer();
-//        String localHost = System.getProperty("myapplication.ip");
-//        client = new GameClient(59687, 5000);
-//        client.connect(localHost);
-//        client.setPlayerOnClient(players[0]);
-//        game = new Game(players, client, 42);
-//        game.init();
-//    }
-//
-//    @Test
-//    public void testAuctionEnter() {
-//        // initialisierung
-//
-//        Auction auc = new Auction(players, client);
-//        AuctionService.startAuction(null);
-//        //Nullpointer?!
-//        Assert.assertTrue("Spieler nicht in Auktion", AuctionService.getAucPlayers() != null);
-//    }
-//
-//}
+package de.btu.monopoly;
+
+//Imports
+import de.btu.monopoly.core.Game;
+import de.btu.monopoly.core.GameBoard;
+import de.btu.monopoly.core.service.AuctionService;
+import de.btu.monopoly.data.card.CardManager;
+import de.btu.monopoly.data.field.FieldManager;
+import de.btu.monopoly.data.field.PropertyField;
+import de.btu.monopoly.data.player.Player;
+import de.btu.monopoly.input.IOService;
+import de.btu.monopoly.menu.Lobby;
+import de.btu.monopoly.menu.LobbyService;
+import de.btu.monopoly.net.client.GameClient;
+import de.btu.monopoly.net.server.GameServer;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author patrick
+ */
+public class AuctionTest {
+
+    private Lobby lobby;
+    private GameServer server;
+    private GameClient client;
+    private Game game;
+    private GameBoard board;
+    private Player[] players;
+    private FieldManager fm;
+    private CardManager cm;
+
+    private void initNetwork() {
+        server = new GameServer(443);
+        server.startServer();
+        client = new GameClient(443, 5000);
+        client.connect(System.getProperty("myapp.ip"));
+    }
+
+    private void initLobby() {
+        initNetwork();
+        lobby = new Lobby();
+        lobby.setHost(true);
+        lobby.setPlayerName("Dummy");
+        lobby.setPlayerClient(client);
+    }
+
+    private void initGame() {
+        initLobby();
+        LobbyService.setLobby(lobby);
+        LobbyService.joinRequest();
+        IOService.sleep(100);
+        LobbyService.addKI("Gegner", 1);
+        IOService.sleep(100);
+
+        Game controller = new Game(LobbyService.generatePlayerArray(), lobby.getPlayerClient(), lobby.getRandomSeed());
+        lobby.setController(controller);
+        lobby.getPlayerClient().setGame(controller);
+        controller.init();
+
+        game = LobbyService.getLobby().getController();
+        board = game.getBoard();
+        players = game.getPlayers();
+        fm = board.getFieldManager();
+        cm = board.getCardManager();
+        IOService.sleep(100);
+    }
+
+    private void clearGame() {
+        client.disconnect();
+        server.stopServer();
+        lobby = null;
+        server = null;
+        client = null;
+        game = null;
+        board = null;
+        players = null;
+        fm = null;
+        cm = null;
+        System.out.println("\nCLEAR GAME ---- ALLES ZURUECKGESETZT!!!\n");
+    }
+
+    @Test
+    public void testAuctionEnter() {
+        // initialisierung
+        initNetwork();
+        initLobby();
+        initGame();
+
+        AuctionService.startAuction((PropertyField) fm.getFields()[1]);
+        //Nullpointer?!
+        Assert.assertTrue("Spieler nicht in Auktion", AuctionService.getAuc().getAucPlayers() != null);
+    }
+
+}
