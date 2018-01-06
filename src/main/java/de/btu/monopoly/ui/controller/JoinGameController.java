c8f4999fb2691452690cb5c8070419e73838771b
Christian Prinz <prinzach@b-tu.de.com>
2018-01-06 14:24:53 +0100

    + Spieler- und KI-namen mit Enter bestätigen

--- src/main/java/de/btu/monopoly/ui/controller/JoinGameController.java
+++ src/main/java/de/btu/monopoly/ui/controller/JoinGameController.java
@@ -1,11 +1,14 @@
 package de.btu.monopoly.ui.controller;
 
+import de.btu.monopoly.input.IOService;
 import de.btu.monopoly.menu.LobbyService;
 import de.btu.monopoly.menu.MainMenu;
 import de.btu.monopoly.ui.SceneManager;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +16,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -57,20 +62,36 @@ public class JoinGameController implements Initializable {
         SceneManager.changeScene(new FXMLLoader(getClass().getResource("/fxml/Menu.fxml")));
     }
 
+    @FXML
+    private void enterJoinsLobby(KeyEvent event) {
+
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            joinLobby();
+        }
+    }
+
     // Button joinLobby
     @FXML
-    private void searchButtonAction(ActionEvent event) throws IOException, InterruptedException {
+    private void searchButtonAction(ActionEvent event) {
+        joinLobby();
+    }
+
+    private void joinLobby() {
         // Server initialisieren
         MainMenu menu = new MainMenu();
         menu.joinGame(ipAdressTextField.getText());
 
         // Namen wechseln
-        Thread.sleep(200);
+        IOService.sleep(200);
         LobbyService.changeName(nameTextField.getText());
 
         if (GuiMessages.getConnectionError() == false) {
-            // Wechselt die Scene auf Lobby
-            SceneManager.changeSceneToLobby(new FXMLLoader(getClass().getResource("/fxml/Lobby.fxml")));
+            try {
+                // Wechselt die Scene auf Lobby
+                SceneManager.changeSceneToLobby(new FXMLLoader(getClass().getResource("/fxml/Lobby.fxml")));
+            } catch (IOException ex) {
+                Logger.getLogger(JoinGameController.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         else {
             errorLabel.setText("Keine Verbindung möglich.");
