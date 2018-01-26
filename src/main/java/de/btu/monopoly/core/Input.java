62bb7c994fea41d550d92a58842dcca6346b0325
Maximilian Bels <belsmaxi@b-tu.de>
2018-01-26 12:48:41 +0000

    + Komponente, die auf lange Sicht den IOService ersetzen und "lokalisieren" soll.

---
+++ src/main/java/de/btu/monopoly/core/Input.java
@@ -0,0 +1,11 @@
+package de.btu.monopoly.core;
+
+/**
+ * Die fachliche Komponente für Spielerentscheidungen.
+ *
+ * @author Maximilian Bels (belsmaxi@b-tu.de)
+ */
+public interface Input {
+    
+    int getInput();
+}
