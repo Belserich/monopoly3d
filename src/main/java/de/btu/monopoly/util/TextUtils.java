12988f4e5389bbb57d2eef8dd9638d125e315732
Maximilian Bels <belsmaxi@b-tu.de>
2018-01-29 12:38:22 +0000

    + Maße für Texturen werden korrekt berechnet + Text-Rendering-Routine überarbeitet

--- src/main/java/de/btu/monopoly/util/TextUtils.java
+++ src/main/java/de/btu/monopoly/util/TextUtils.java
@@ -1,85 +1,60 @@
 package de.btu.monopoly.util;
 
+import de.btu.monopoly.data.field.Field;
+import de.btu.monopoly.data.field.PropertyField;
+import de.btu.monopoly.data.field.TaxField;
 import de.btu.monopoly.ui.fx3d.Fx3dFieldType;
-import de.btu.monopoly.ui.fx3d.Fx3dGameBoard;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
+import javafx.geometry.VPos;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
-import javafx.scene.transform.Translate;
 
-import static de.btu.monopoly.ui.fx3d.Fx3dField.FIELD_HEIGHT;
-import static de.btu.monopoly.ui.fx3d.Fx3dField.FIELD_WIDTH;
+import static de.btu.monopoly.ui.fx3d.Fx3dField.*;
 
 public class TextUtils
 {
-    private static final Rotate TEXT_ROTATE = new Rotate(-90, Rotate.X_AXIS);
-    private static final Transform CORNER_TEXT_ROTATE = TEXT_ROTATE;
-    
     private static final String FIELD_NAME_KEY = "field_%d_name";
     private static final String FIELD_PRICE_KEY = "field_%d_price";
     
     private static final double LINE_WRAPPING_WIDTH = FIELD_WIDTH - 10;
     
-    private static final Transform FIELD_NAME_TRANSFORM_0 = new Translate(0, -FIELD_HEIGHT, 20).createConcatenation(TEXT_ROTATE);
-    private static final Transform FIELD_NAME_TRANSFORM_1 = new Translate(0, -FIELD_HEIGHT, 55).createConcatenation(TEXT_ROTATE);
-    private static final Transform FIELD_PRICE_TRANSFORM = new Translate(0, -FIELD_HEIGHT, -70).createConcatenation(TEXT_ROTATE);
+    private static final double FIELD_TEX_CENTER_X = FIELD_HEIGHT + FIELD_WIDTH / 2;
+    private static final double FIELD_TEX_CENTER_Y = FIELD_HEIGHT + FIELD_DEPTH / 2;
     
-    private static final Transform BASIC_CORNER_TRANSFORM = TEXT_ROTATE.createConcatenation(CORNER_TEXT_ROTATE);
+    private static final double FIELD_MAX_TEXT_WIDTH = FIELD_WIDTH - 10;
     
-    public static Group createFieldTexts(int fieldId)
+    private static final double STREET_NAME_OFF = -25;
+    private static final double STREET_PRICE_OFF = 2.7 * -STREET_NAME_OFF;
+    
+    public static void drawText(GraphicsContext context, Field field, Fx3dFieldType type)
     {
-        String str = Assets.getString(String.format(FIELD_NAME_KEY, fieldId));
-        Group group = new Group();
+        context.setFont(Font.font("Kabel", 14));
+        context.setTextAlign(TextAlignment.CENTER);
+        context.setTextBaseline(VPos.CENTER);
+        context.setLineWidth(FIELD_MAX_TEXT_WIDTH);
+        context.translate(FIELD_TEX_CENTER_X, FIELD_TEX_CENTER_Y);
         
-        Fx3dFieldType type = Fx3dFieldType.GAMEBOARD_FIELD_STRUCTURE[fieldId];
-        if (!type.isCorner() && !type.isCard())
-        {
-            Text name = createText(str);
-            name.getTransforms().add(type.isStreet() ? FIELD_NAME_TRANSFORM_0 : FIELD_NAME_TRANSFORM_1);
-    
-            Text price = createText(Assets.getString(String.format(FIELD_PRICE_KEY, fieldId)));
-            price.getTransforms().add(FIELD_PRICE_TRANSFORM);
+        if (type.isStreet() || type.isStation() || type.isSupply()) {
+            PropertyField prop = (PropertyField) field;
+            context.fillText(String.valueOf(prop.getPrice()), 0, STREET_PRICE_OFF);
             
-            group.getChildren().addAll(name, price);
-        }
-        else if (type.isCard())
-        {
-            Text name = createText(str);
-            name.getTransforms().add(FIELD_NAME_TRANSFORM_1);
-            group.getChildren().add(name);
-        }
-        else
-        {
-            int delim = str.lastIndexOf(' ');
-            delim = delim == -1 ? 0 : delim;
-            if (type == Fx3dFieldType.CORNER_0)
-            {
-                Text text = createText(str.substring(0, delim));
-                Text name = createText(str.substring(delim));
-                
-                group.getChildren().addAll(text, name);
-                group.getTransforms().add(FIELD_NAME_TRANSFORM_1);
+            if (type.isStreet()) {
+                context.fillText(prop.getName(), 0, STREET_NAME_OFF);
             }
-            else group.getChildren().add(createText(str));
+            else if (type.isStation() || type.isSupply()) {
+                context.setFont(Font.font("Kabel", 16));
+                context.fillText(prop.getName(), 0, STREET_NAME_OFF - 35);
+            }
         }
-        
-        return group;
-    }
-    
-    private static void rangeCheck(int fieldId)
-    {
-        if (fieldId < 0 || fieldId > Fx3dGameBoard.FIELD_COUNT)
-            throw new IllegalArgumentException(String.format("id (%d) out of range!", fieldId));
-    }
-    
-    private static Text createText(String content)
-    {
-        Text text = new Text(content);
-        text.setWrappingWidth(LINE_WRAPPING_WIDTH);
-        text.setTextAlignment(TextAlignment.CENTER);
-        text.setTranslateX(-text.getLayoutBounds().getWidth() / 2);
-        return text;
+        else if (type.isCard() || type.isTax()) {
+            
+            if (type.isTax()) {
+                TaxField tax = (TaxField) field;
+                context.fillText(String.valueOf(tax.getTax()), 0, STREET_PRICE_OFF);
+            }
+            
+            context.setFont(Font.font("Kabel", 16));
+            context.fillText(field.getName(), 0, STREET_NAME_OFF - 35);
+        }
     }
 }
